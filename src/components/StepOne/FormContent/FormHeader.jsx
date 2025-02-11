49868a612dc3668122ec64cb5a52dd2ff2ae32ed
Tom Okafor <tmeka008@gmail.com>
2025-02-11 09:15:44 +0100

    feat: add text and details to step one form header and form title

--- src/components/StepOne/FormContent/FormHeader.jsx
+++ src/components/StepOne/FormContent/FormHeader.jsx
@@ -1,3 +1,16 @@
 export default function FormHeader() {
-    return <div className="form-header"></div>
+  return (
+    <div className="form-header">
+      <div className="form-header-text-content">
+        <p className="form-header-title">ticket selection</p>
+        <p className="form-header-steps-counter">Step 1/3</p>
+      </div>
+      <div className="form-header-progress">
+        <div
+          className="form-header-progress-bar"
+          style={{ width: `${(1 / 3) * 100}%` }}
+        ></div>
+      </div>
+    </div>
+  );
 }
--- src/components/StepOne/FormContent/FormTitle.jsx
+++ src/components/StepOne/FormContent/FormTitle.jsx
@@ -1,3 +1,18 @@
-export default function FormTitle(){
-    return <div className="form-title"></div>
+export default function FormTitle() {
+  return (
+    <div className="form-title">
+      <div className="form-title-main">
+        <p className="form-title-main-heading">Techember Fest ”25</p>
+        <p className="form-title-main-text">
+          Join us for an unforgettable experience at Techember Fest! Secure your
+          spot now.
+        </p>
+      </div>
+      <div className="form-title-details">
+        <span>📍 Ikoyi, Lagos</span>
+        <span>| |</span>
+        <span>March 15, 2025 | 7:00 PM</span>
+      </div>
+    </div>
+  );
 }
