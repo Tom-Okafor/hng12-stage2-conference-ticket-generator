4090fad7aaec764c2d20ac7603454e364430683a
Tom Okafor <tmeka008@gmail.com>
2025-02-13 00:41:28 +0100

    remove empty src attribute to sort out console error.

--- src/components/StepThree/StepThreeTicketImage.jsx
+++ src/components/StepThree/StepThreeTicketImage.jsx
@@ -1,5 +1,5 @@
 import '../../styles/StepThreeStyles/StepThreeTicketImage.css'
 
 export default function StepThreeTicketImage() {
-    return <img src="" alt="Profile Image" className='ticket-image' />
+    return <img  alt="Profile Image" className='ticket-image' />
 }
