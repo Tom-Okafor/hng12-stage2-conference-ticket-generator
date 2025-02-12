94e64f2b6c4bac78d5ff19ff04623b944f800d94
Tom Okafor <tmeka008@gmail.com>
2025-02-12 18:19:29 +0100

    add ticket background image in svg format to the public folder and set it as the background image of the step three ticket

---
+++ src/components/StepThree/StepThreeTicket.jsx
@@ -0,0 +1,5 @@
+import "../../styles/StepThreeStyles/StepThreeTicket.css";
+
+export default function StepThreeTicket() {
+  return <div className="step-three-ticket"></div>;
+}
--- src/components/StepThree/StepThreeTicketContainer.jsx
+++ src/components/StepThree/StepThreeTicketContainer.jsx
@@ -1,5 +1,10 @@
 import "../../styles/StepThreeStyles/StepThreeTicketContainer.css";
+import StepThreeTicket from "./StepThreeTicket";
 
 export default function StepThreeTicketContainer() {
-    return <div className="step-three-ticket-container"></div>
+  return (
+    <div className="step-three-ticket-container">
+      <StepThreeTicket />
+    </div>
+  );
 }
