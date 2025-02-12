7541b6dd4393f69b1dcfc6cff5c934ad36b6611c
Tom Okafor <tmeka008@gmail.com>
2025-02-12 16:08:43 +0100

    add aria-labelledby to text input in step two form.

--- src/components/StepTwo/StepTwoFormContent/StepTwoNameInput.jsx
+++ src/components/StepTwo/StepTwoFormContent/StepTwoNameInput.jsx
@@ -3,8 +3,8 @@ import '../../../styles/StepTwoStyles/inputSection.css'
 export default function StepTwoNameInput() {
   return (
     <section className="name-input-holder input-section">
-      <label htmlFor="name-input">Enter your name</label>
-      <input type="text" name="name-input" id="name-input" className="input" required />
+      <label htmlFor="name-input" id="text-label">Enter your name</label>
+      <input type="text" name="name-input" id="name-input" className="input" required aria-labelledby="text-label"/>
     </section>
   );
 }
