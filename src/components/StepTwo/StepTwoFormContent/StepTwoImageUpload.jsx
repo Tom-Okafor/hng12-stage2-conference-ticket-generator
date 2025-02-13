75fdd633b1e003f9a2193a7c49b64fd141d1470b
Tom Okafor <tmeka008@gmail.com>
2025-02-13 06:08:39 +0100

    refactor: change upload image input structure to allow for more flexible design.

--- src/components/StepTwo/StepTwoFormContent/StepTwoImageUpload.jsx
+++ src/components/StepTwo/StepTwoFormContent/StepTwoImageUpload.jsx
@@ -1,6 +1,7 @@
 import "../../../styles/StepTwoStyles/stepTwoImageUpload.css";
 
 export default function StepTwoImageUpload() {
+  const condition = false;
   return (
     <div className="image-upload-box">
       <p className="image-upload-label">Upload Profile Photo</p>
@@ -12,10 +13,19 @@ export default function StepTwoImageUpload() {
             id="upload-input"
             accept="image/*"
           />
-          <img src="icon.png" alt="icon" aria-hidden="true" />
-          <p className="upload-instructions">
-            Drag and drop or click to upload
-          </p>
+          <div className="upload-input-innerbox">
+            <img src="icon.png" alt="icon" aria-hidden="true" />
+            <p className="upload-instructions">
+              Drag and drop or click to upload
+            </p>
+          </div>
+          {condition && (
+            <img
+              src="https://th.bing.com/th/id/R.18f14463a91f8316ec8daea09ab5baaf?rik=1ONxPv6onaga7A&pid=ImgRaw&r=0"
+              alt="image"
+              className="profile-photo uploaded"
+            />
+          )}
         </div>
       </div>
     </div>
