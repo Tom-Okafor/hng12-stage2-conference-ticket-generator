cc0877d101df5b90da836b1fc402e523064970dc
Tom Okafor <tmeka008@gmail.com>
2025-02-14 00:59:12 +0100

    create structure for about page and add styles.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,30 @@
 import Header from "../../components/General/Header";
+import { aboutPageText } from "../../constants";
 
 export default function About() {
   return (
-    <main>
+    <main className="about">
       <Header />
-      <h1 style={{ color: "white" }}>About Page Here</h1>
+      <section className="about-content">
+        <p className="about-text">{aboutPageText}</p>
+        <p className="fun-text">💛 Enjoy</p>
+        <div className="about-links">
+          <a
+            href="https://www.figma.com/community/file/1470800949188681164/event-ticket-booking-ui-open-source-practice-project"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Design File
+          </a>
+          <a
+            href="https://github.com/Tom-Okafor/hng12-stage2-conference-ticket-generator"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            Github Code
+          </a>
+        </div>
+      </section>
     </main>
   );
 }
