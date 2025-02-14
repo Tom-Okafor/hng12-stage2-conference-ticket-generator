2e9e847f108b96de0a929fac08d0363cab47a28b
Tom Okafor <tmeka008@gmail.com>
2025-02-14 15:56:27 +0100

    fix: correct casing error in import

--- src/components/StepTwo/StepTwo.jsx
+++ src/components/StepTwo/StepTwo.jsx
@@ -1,6 +1,6 @@
 import "../../styles/StepOneStyles/general.css";
 import Header from "../General/Header";
-import StepTwoFormContent from "./StepTwoFormContent/stepTwoFormContent";
+import StepTwoFormContent from "./StepTwoFormContent/StepTwoFormContent";
 export default function StepTwo() {
   return (
     <section className="step-two">
